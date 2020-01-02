d6917701817ed89e949619203482911f7921983c
“X <wsx05@163.com>
2020-01-02 21:25:12 +0800

    update .gitignore

--- Ui/MFZ/debug/InstallRemindMessageService.bat
+++
@@ -1,3 +0,0 @@
-C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\installutil /u EMR_RemindMessageService.exe
-C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\installutil EMR_RemindMessageService.exe
-pause
